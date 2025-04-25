c3fd54f51d1b339ddd348cd4f86cba9c46116592
suhel <suhelkhan87514@gmail.com>
2025-04-25 13:07:08 +0530

    added chat comp

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/component/chat/Main";
 
 export default function Home() {
   return (
-    <div className="flex flex-row w-screen justify-between h-screen">
+    <div className="flex flex-row w-screen items-center justify-between h-screen">
       <h1 className="w-1/2">hi</h1>
       <Chat />
     </div>
